fbe4d541e2b02c5669fe47485d408a7dfc61070c
Petr Šrámek <petr.sramek@dropoutcoder.com>
2022-10-15 16:36:34 +0200

    Added i9nitial rental to database

--- src/SmartRental/Services/InitializeDatabaseHostingService.cs
+++ src/SmartRental/Services/InitializeDatabaseHostingService.cs
@@ -1,4 +1,5 @@
 ﻿using SmartRental.Infrastructure.Database.Abstraction;
+using SmartRental.Infrastructure.Database.ComplexTypes;
 
 namespace SmartRental.Services
 {
@@ -17,12 +18,20 @@ namespace SmartRental.Services
             {
                 var store = scope.ServiceProvider.GetRequiredService<ICarStore>();
                 await store.AddCarAsync("SKODA1", "Skoda Fabia");
+                await store.AddCarAsync("SKODA2", "Skoda Superb");
             }
 
             using (var scope = Provider.CreateScope())
             {
                 var store = scope.ServiceProvider.GetRequiredService<ICustomerStore>();
                 await store.AddCustomerAsync("Petr", "Sramek", "Marsaskala", "MSK 4042", "Id-Dugh", DateTime.Now);
+
+            }
+
+            using (var scope = Provider.CreateScope())
+            {
+                var store = scope.ServiceProvider.GetRequiredService<IRentalStore>();
+                await store.AddRentalAsync(1, 1, "123456",  PersonalIdentification.Create(123456, PersonalIdentification.IdentificationType.NationalId), DateTime.Now, DateTime.Now.AddDays(4), 120);
 
             }
         }
